b41fd6de83552b9b363f94b9fe582f375ca1a4e0
pressupinc <pressupinc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 11:43:37 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/display-all-image-sizes/trunk@1170090 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display-all-image-sizes.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: Display All Image Sizes
+Description: Makes every uploaded image's attachment page display all of the image's sizes, including name, dimension, and permalink for each size. A major time-saver if you frequently use custom-generated image sizes.
+Author: Press Up
+Version: 1.0.1
+Author URI: http://pressupinc.com/
+Text Domain: display-all-image-sizes
+*/
+
+add_action( 'admin_enqueue_scripts', 'wpshout_enqueue_display_all_image_sizes_stylesheet' );
+function wpshout_enqueue_display_all_image_sizes_stylesheet() {
+	wp_enqueue_style('display-all-image-sizes', plugin_dir_url( __FILE__ ) . 'display-all-image-sizes.css');
+}
+
+add_filter( 'attachment_fields_to_edit', 'wpshout_display_all_image_sizes', 10, 2 );
+function wpshout_display_all_image_sizes( $form_fields, $post ) {
+	$size_names = get_intermediate_image_sizes( $post->ID );
+
+	// Returns array of sizes, each containing array of name, link, length, height
+	$sizes = wpshout_return_sizes( $size_names, $post->ID );
+
+	if( ! is_array($sizes) ) {
+		return $form_fields;
+	}
+
+	$size_data_html = wpshout_build_size_data_html($sizes);
+
+	if( ! isset($size_data_html) || $size_data_html == '') {
+		return $form_fields;
+	}
+
+    $form_fields['sizes'] = array (
+        'label' => __( 'Sizes', 'display-all-image-sizes' ),
+        'input' => 'html',
+		'html' => $size_data_html,
+    );
+
+    return $form_fields;
+}
+
+/* Return array of size data
+* Thanks to Justin Tadlock:
+http://justintadlock.com/archives/2011/01/28/linking-to-all-image-sizes-in-wordpress */
+function wpshout_return_sizes( $size_names, $post_id ) {
+
+	/* Get the intermediate image sizes and add the full size to the array. */
+	if( ! is_array($size_names) ) {
+		return false;
+	}
+
+	$size_names[] = 'full';
+
+	/* Loop through each of the image sizes. */
+	foreach ( $size_names as $size_name ) {
+
+		/* Get the image source, width, height, and whether it's intermediate. */
+		$image = wp_get_attachment_image_src( $post_id, $size_name );
+
+		/* Return name, link, width, height */
+		if ( !empty( $image ) && ( true == $image[3] || 'full' == $size_name ) ) {
+			$size_data[] = array ( 
+				'name' => $size_name, 
+				'link' => $image[0],
+				'width' => $image[1],
+				'height' => $image[2]
+			);
+		}
+	}
+
+	/* Return if array */
+	if (is_array($size_data) && count($size_data) > 0) {
+		return $size_data;
+	}
+}
+
+/* Build HTML size data string based on array of size data */
+function wpshout_build_size_data_html($sizes) {
+	if( ! is_array($sizes)) {
+		return;
+	}
+
+	foreach($sizes as $key => $value) {
+		$size_data_html .= $value['name'] . ': ' .
+		$value['width'] . 'x' . $value['height'] . '<br>' .
+		'<input type="text" value="' . $value['link'] . '" readonly>' .
+		'<br><br>';
+	}
+
+	return $size_data_html;
+}
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Display All Image Sizes ===
+Contributors: pressupinc
+Tags: image sizes, picture sizes, view image sizes, view image size links, different image sizes, thumbnails, thumbnail sizes, featured images, featured image sizes, attachment sizes
+Requires at least: 3.5
+Tested up to: 4.2.2
+Stable tag: 1.0.1
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+
+Requires content you specify to have a featured image set before they can be published.
+
+== Description ==
+
+= View and Use Every Size of Your Images =
+
+When you upload an image to your WordPress site, WordPress auto-generates lots of sizes of it. Unfortunately, the Media Attachment page doesn't display any information about the different image sizes, or the permalinks to use them. And the Attachment Display settings inside "Add Media" doesn't always show you all the sizes that exist. Until now.
+
+= How to Use =
+
+Just install and activate the plugin, and you'll see a list of all sizes of each image in both the "Add Media" pop-up modal, and in each image's individual page in "Media."
+
+= Anything else? =
+
+Well, read [read WPShout](http://wpshout.com/) for WordPress news and tutorials! And [contact us](http://pressupinc.com/) if you need help with WordPress.
+
+== Installation ==
+
+Install and activate the plugin. It'll work by default, and doesn't register any options pages.
+
+== Frequently Asked Questions ==
+
+= Got a Question? =
+
+Let us know, happy to help!
+
+== Screenshots ==
+
+1. Sizes on the "Insert Media" pop-up modal.
+
+2. Sizes on "Attachment Details" in Media.
+
+3. Sizes on "Edit Attachment." 
+
+== CHANGELOG ==
+
+= 1.0.1 (27.05.2015) =
+* Initial commit
